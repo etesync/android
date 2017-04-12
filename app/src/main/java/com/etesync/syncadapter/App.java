57f447d372306499db978e9360417418e2ebda5e
Tom Hacohen <tom@stosb.com>
2017-04-12 18:44:39 +0100

    Requery: Don't destroy and recreate db when updating.

--- app/src/main/java/com/etesync/syncadapter/App.java
+++ app/src/main/java/com/etesync/syncadapter/App.java
@@ -228,10 +228,6 @@ public class App extends Application {
         if (dataStore == null) {
             // override onUpgrade to handle migrating to a new version
             DatabaseSource source = new DatabaseSource(this, Models.DEFAULT, 1);
-            if (BuildConfig.DEBUG) {
-                // use this in development mode to drop and recreate the tables on every upgrade
-                source.setTableCreationMode(TableCreationMode.DROP_CREATE);
-            }
             Configuration configuration = source.getConfiguration();
             dataStore = new EntityDataStore<>(configuration);
         }
