c3ee3aac22e8935e1dfd64a99a138d4a8688d371
Tom Hacohen <tom@stosb.com>
2017-05-11 22:33:44 +0100

    Journal item: Implement showing contacts.
    This shows address book journal items in a nicer way.

--- app/src/main/java/com/etesync/syncadapter/ui/JournalItemActivity.java
+++ app/src/main/java/com/etesync/syncadapter/ui/JournalItemActivity.java
@@ -33,10 +33,23 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Formatter;
+import java.util.List;
 import java.util.Locale;
 
 import at.bitfire.ical4android.Event;
 import at.bitfire.ical4android.InvalidCalendarException;
+import at.bitfire.vcard4android.Contact;
+import at.bitfire.vcard4android.LabeledProperty;
+import ezvcard.parameter.AddressType;
+import ezvcard.parameter.EmailType;
+import ezvcard.parameter.RelatedType;
+import ezvcard.parameter.TelephoneType;
+import ezvcard.property.Address;
+import ezvcard.property.Email;
+import ezvcard.property.Impp;
+import ezvcard.property.Related;
+import ezvcard.property.Telephone;
+import ezvcard.property.Url;
 import io.requery.Persistable;
 import io.requery.sql.EntityDataStore;
 
@@ -120,7 +133,7 @@ public class JournalItemActivity extends BaseActivity implements Refreshable {
         @Override
         public Fragment getItem(int position) {
             if (position == 0) {
-                return EventFragment.newInstance(info, syncEntry);
+                return PrettyFragment.newInstance(info, syncEntry);
             } else {
                 return TextFragment.newInstance(syncEntry);
             }
@@ -149,12 +162,12 @@ public class JournalItemActivity extends BaseActivity implements Refreshable {
         }
     }
 
-    public static class EventFragment extends Fragment {
+    public static class PrettyFragment extends Fragment {
         CollectionInfo info;
         SyncEntry syncEntry;
 
-        public static EventFragment newInstance(CollectionInfo info, SyncEntry syncEntry) {
-            EventFragment frag = new EventFragment();
+        public static PrettyFragment newInstance(CollectionInfo info, SyncEntry syncEntry) {
+            PrettyFragment frag = new PrettyFragment();
             Bundle args = new Bundle(1);
             args.putSerializable(Constants.KEY_COLLECTION_INFO, info);
             args.putSerializable(KEY_SYNC_ENTRY, syncEntry);
@@ -164,12 +177,21 @@ public class JournalItemActivity extends BaseActivity implements Refreshable {
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            View v = inflater.inflate(R.layout.event_info, container, false);
+            View v = null;
 
             info = (CollectionInfo) getArguments().getSerializable(Constants.KEY_COLLECTION_INFO);
             syncEntry = (SyncEntry) getArguments().getSerializable(KEY_SYNC_ENTRY);
 
-            new LoadEventTask(v).execute();
+            switch (info.type) {
+                case ADDRESS_BOOK:
+                    v = inflater.inflate(R.layout.contact_info, container, false);
+                    new LoadContactTask(v).execute();
+                    break;
+                case CALENDAR:
+                    v = inflater.inflate(R.layout.event_info, container, false);
+                    new LoadEventTask(v).execute();
+                    break;
+            }
 
             return v;
         }
@@ -185,8 +207,7 @@ public class JournalItemActivity extends BaseActivity implements Refreshable {
                 InputStream is = new ByteArrayInputStream(syncEntry.getContent().getBytes(Charsets.UTF_8));
 
                 try {
-                    Event event = Event.fromStream(is, Charsets.UTF_8, null)[0];
-                    return event;
+                    return Event.fromStream(is, Charsets.UTF_8, null)[0];
                 } catch (InvalidCalendarException e) {
                     e.printStackTrace();
                 } catch (IOException e) {
@@ -245,6 +266,125 @@ public class JournalItemActivity extends BaseActivity implements Refreshable {
                 }
                 setTextViewText(view, R.id.reminders, sb.toString());
             }
+        }
+
+        private class LoadContactTask extends AsyncTask<Void, Void, Contact> {
+            View view;
+
+            LoadContactTask(View v) {
+                super();
+                view = v;
+            }
+
+            @Override
+            protected Contact doInBackground(Void... aVoids) {
+                InputStream is = new ByteArrayInputStream(syncEntry.getContent().getBytes(Charsets.UTF_8));
+
+                try {
+                    return Contact.fromStream(is, Charsets.UTF_8, null)[0];
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Contact contact) {
+                final View loader = view.findViewById(R.id.loading_msg);
+                loader.setVisibility(View.GONE);
+                final View contentContainer = view.findViewById(R.id.content_container);
+                contentContainer.setVisibility(View.VISIBLE);
+
+                TextView tv = (TextView) view.findViewById(R.id.display_name);
+                tv.setText(contact.displayName);
+
+                final ViewGroup mainCard = (ViewGroup) view.findViewById(R.id.main_card);
+                final ViewGroup aboutCard = (ViewGroup) view.findViewById(R.id.about_card);
+                aboutCard.findViewById(R.id.title_container).setVisibility(View.VISIBLE);
+
+                // TEL
+                for (LabeledProperty<Telephone> labeledPhone : contact.phoneNumbers) {
+                    List<TelephoneType> types = labeledPhone.property.getTypes();
+                    String type = (types.size() > 0) ? types.get(0).getValue() : null;
+                    addInfoItem(view.getContext(), mainCard, "Phone", type, labeledPhone.property.getText());
+                }
+
+                // EMAIL
+                for (LabeledProperty<Email> labeledEmail : contact.emails) {
+                    List<EmailType> types = labeledEmail.property.getTypes();
+                    String type = (types.size() > 0) ? types.get(0).getValue() : null;
+                    addInfoItem(view.getContext(), mainCard, "Email", type, labeledEmail.property.getValue());
+                }
+
+                // ORG, TITLE, ROLE
+                if (contact.organization != null) {
+                    addInfoItem(view.getContext(), aboutCard, "Organization", contact.jobTitle, contact.organization.getValues().get(0));
+                }
+                if (contact.jobDescription != null) {
+                    addInfoItem(view.getContext(), aboutCard, "Job Description", null, contact.jobTitle);
+                }
+
+                // IMPP
+                for (LabeledProperty<Impp> labeledImpp : contact.impps) {
+                    addInfoItem(view.getContext(), mainCard, "Instant Messaging", labeledImpp.property.getProtocol(), labeledImpp.property.getHandle());
+                }
+
+                // NICKNAME
+                if (contact.nickName != null) {
+                    addInfoItem(view.getContext(), aboutCard, "Nickname", null, contact.nickName.getValues().get(0));
+                }
+
+                // ADR
+                for (LabeledProperty<Address> labeledAddress : contact.addresses) {
+                    List<AddressType> types = labeledAddress.property.getTypes();
+                    String type = (types.size() > 0) ? types.get(0).getValue() : null;
+                    addInfoItem(view.getContext(), mainCard, "Address", type, labeledAddress.property.getLabel());
+                }
+
+                // NOTE
+                if (contact.note != null) {
+                    addInfoItem(view.getContext(), aboutCard, "Note", null, contact.note);
+                }
+
+                // URL
+                for (LabeledProperty<Url> labeledUrl : contact.urls) {
+                    addInfoItem(view.getContext(), aboutCard, "Website", null, labeledUrl.property.getValue());
+                }
+
+                // ANNIVERSARY
+                if (contact.anniversary != null) {
+                    String date = getDisplayedDatetime(contact.anniversary.getDate().getTime(), contact.anniversary.getDate().getTime(), true, getContext());
+                    addInfoItem(view.getContext(), aboutCard, "Anniversary", null, date);
+                }
+                // BDAY
+                if (contact.birthDay != null) {
+                    String date = getDisplayedDatetime(contact.birthDay.getDate().getTime(), contact.birthDay.getDate().getTime(), true, getContext());
+                    addInfoItem(view.getContext(), aboutCard, "Birthday", null, date);
+                }
+
+                // RELATED
+                for (Related related : contact.relations) {
+                    List<RelatedType> types = related.getTypes();
+                    String type = (types.size() > 0) ? types.get(0).getValue() : null;
+                    addInfoItem(view.getContext(), aboutCard, "Relation", type, related.getText());
+                }
+
+                // PHOTO
+                // if (contact.photo != null)
+            }
+        }
+
+        private static View addInfoItem(Context context, ViewGroup parent, String type, String label, String value) {
+            ViewGroup layout = (ViewGroup) parent.findViewById(R.id.container);
+            View infoItem = LayoutInflater.from(context).inflate(R.layout.contact_info_item, layout, false);
+            layout.addView(infoItem);
+            setTextViewText(infoItem, R.id.type, type);
+            setTextViewText(infoItem, R.id.title, label);
+            setTextViewText(infoItem, R.id.content, value);
+            parent.setVisibility(View.VISIBLE);
+
+            return infoItem;
         }
 
         private static void setTextViewText(View parent, int id, String text) {
