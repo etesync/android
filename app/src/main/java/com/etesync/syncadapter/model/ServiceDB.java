etesync/android

commit df94fde5ff45a1aae6ff0a3abc76241b546feefa

Move CollectionInfo and SyncEntry to the journalmanager module.

app/src/main/java/com/etesync/syncadapter/model/ServiceDB.java
@@ -92,7 +92,7 @@ public class ServiceDB {
             db.beginTransactionNonExclusive();
 
             // iterate through all tables
-            Cursor cursorTables = db.query("sqlite_master", new String[]{"name"}, "type='table'", null, null, null, null);
+            Cursor cursorTables = db.query("sqlite_master", new String[]{"name"}, "enumType='table'", null, null, null, null);
             while (cursorTables.moveToNext()) {
                 String table = cursorTables.getString(0);
                 sb.append(table).append("\n");

app/build.gradle
@@ -133,7 +133,7 @@ dependencies {
     implementation "org.jetbrains.kotlin:kotlin-stdlib-jdk7:$kotlin_version"
     implementation "org.jetbrains.anko:anko-commons:0.10.4"
 
-    implementation "com.etesync:journalmanager:1.0.1"
+    implementation "com.etesync:journalmanager:1.0.2"
 
     def acraVersion = '5.3.0'
     implementation "ch.acra:acra-mail:$acraVersion"

app/src/main/java/com/etesync/syncadapter/model/CollectionInfo.kt
@@ -9,40 +9,18 @@
 package com.etesync.syncadapter.model
 
 import android.content.ContentValues
-import com.etesync.journalmanager.Constants
-import com.etesync.journalmanager.JournalManager
 import com.etesync.syncadapter.model.ServiceDB.Collections
 import com.google.gson.GsonBuilder
-import com.google.gson.annotations.Expose
-import java.io.Serializable
 
-class CollectionInfo : Serializable {
+class CollectionInfo : com.etesync.journalmanager.model.CollectionInfo() {
     @Deprecated("")
     var id: Long = 0
 
     var serviceID: Int = 0
 
-    // FIXME: Shouldn't be exposed, as it's already saved in the journal. We just expose it for when we save for db.
-    @Expose
-    var version = -1
-
-    @Expose
-    var type: Type? = null
-
-    var uid: String? = null
-
-    @Expose
-    var displayName: String? = null
-    @Expose
-    var description: String? = null
-    @Expose
-    var color: Int? = null
-
-    @Expose
-    var timeZone: String? = null
-
-    @Expose
-    var selected: Boolean = false
+    fun getServiceEntity(data: MyEntityDataStore): ServiceEntity {
+        return data.findByKey(ServiceEntity::class.java, serviceID)
+    }
 
     enum class Type {
         ADDRESS_BOOK,
@@ -50,38 +28,18 @@ class CollectionInfo : Serializable {
         TASKS,
     }
 
-    init {
-        version = Constants.CURRENT_VERSION
-    }
-
-    fun updateFromJournal(journal: JournalManager.Journal) {
-        uid = journal.uid!!
-        version = journal.version
-    }
-
-    fun isOfTypeService(service: String): Boolean {
-        return service == type.toString()
-    }
-
-    fun getServiceEntity(data: MyEntityDataStore): ServiceEntity {
-        return data.findByKey(ServiceEntity::class.java, serviceID)
-    }
-
-    fun toJson(): String {
-        return GsonBuilder().excludeFieldsWithoutExposeAnnotation().create().toJson(this, CollectionInfo::class.java)
-    }
-
-    override fun toString(): String {
-        return "CollectionInfo(serviceID=" + this.serviceID + ", version=" + this.version + ", type=" + this.type + ", uid=" + this.uid + ", displayName=" + this.displayName + ", description=" + this.description + ", color=" + this.color + ", timeZone=" + this.timeZone + ", selected=" + this.selected + ")"
-    }
+    var enumType: Type?
+        get() = if (super.type != null) Type.valueOf(super.type!!) else null
+        set(value) {
+            super.type = value?.name
+        }
 
     companion object {
-
         fun defaultForServiceType(service: Type): CollectionInfo {
             val info = CollectionInfo()
             info.displayName = "Default"
             info.selected = true
-            info.type = service
+            info.enumType = service
 
             return info
         }

app/src/main/java/com/etesync/syncadapter/model/JournalModel.java
@@ -1,5 +1,7 @@
 package com.etesync.syncadapter.model;
 
+import com.etesync.journalmanager.model.SyncEntry;
+
 import java.util.List;
 
 import io.requery.Column;
@@ -11,11 +13,9 @@ import io.requery.Generated;
 import io.requery.Index;
 import io.requery.Key;
 import io.requery.ManyToOne;
-import io.requery.Persistable;
 import io.requery.PostLoad;
 import io.requery.ReferentialAction;
 import io.requery.Table;
-import io.requery.sql.EntityDataStore;
 
 public class JournalModel {
     @Entity

app/src/main/java/com/etesync/syncadapter/model/SyncEntry.kt
@@ -1,52 +1,0 @@
-package com.etesync.syncadapter.model
-
-import com.etesync.syncadapter.GsonHelper
-import com.etesync.journalmanager.Crypto
-import com.etesync.journalmanager.JournalEntryManager
-
-import java.io.Serializable
-
-class SyncEntry : Serializable {
-    val content: String
-    val action: Actions
-
-    enum class Actions constructor(private val text: String) {
-        ADD("ADD"),
-        CHANGE("CHANGE"),
-        DELETE("DELETE");
-
-        override fun toString(): String {
-            return text
-        }
-    }
-
-    private constructor() {
-        this.content = ""
-        this.action = Actions.ADD
-    }
-
-    constructor(content: String, action: Actions) {
-        this.content = content
-        this.action = action
-    }
-
-    fun isAction(action: Actions): Boolean {
-        return this.action == action
-    }
-
-    fun toJson(): String {
-        return GsonHelper.gson.toJson(this, this.javaClass)
-    }
-
-    companion object {
-        @JvmStatic
-        fun fromJournalEntry(crypto: Crypto.CryptoManager, entry: JournalEntryManager.Entry): SyncEntry {
-            return fromJson(entry.getContent(crypto))
-        }
-
-        @JvmStatic
-        fun fromJson(json: String): SyncEntry {
-            return GsonHelper.gson.fromJson(json, SyncEntry::class.java)
-        }
-    }
-}

app/src/main/java/com/etesync/syncadapter/syncadapter/AccountAuthenticatorService.kt
@@ -25,7 +25,7 @@ import java.util.logging.Level
 
 
 /**
- * Account authenticator for the main DAVx5 account type.
+ * Account authenticator for the main DAVx5 account enumType.
  *
  * Gets started when a DAVx5 account is removed, too, so it also watches for account removals
  * and contains the corresponding cleanup code.

app/src/main/java/com/etesync/syncadapter/syncadapter/CalendarSyncManager.kt
@@ -24,7 +24,7 @@ import com.etesync.journalmanager.Exceptions
 import com.etesync.journalmanager.JournalEntryManager
 import com.etesync.syncadapter.log.Logger
 import com.etesync.syncadapter.model.CollectionInfo
-import com.etesync.syncadapter.model.SyncEntry
+import com.etesync.journalmanager.model.SyncEntry
 import com.etesync.syncadapter.resource.LocalCalendar
 import com.etesync.syncadapter.resource.LocalEvent
 import com.etesync.syncadapter.utils.EventEmailInvitation

app/src/main/java/com/etesync/syncadapter/syncadapter/ContactsSyncManager.kt
@@ -24,7 +24,7 @@ import com.etesync.journalmanager.Exceptions
 import com.etesync.journalmanager.JournalEntryManager
 import com.etesync.syncadapter.log.Logger
 import com.etesync.syncadapter.model.CollectionInfo
-import com.etesync.syncadapter.model.SyncEntry
+import com.etesync.journalmanager.model.SyncEntry
 import com.etesync.syncadapter.resource.LocalAddress
 import com.etesync.syncadapter.resource.LocalAddressBook
 import com.etesync.syncadapter.resource.LocalContact

commit d603245eb373c665042e28f2a660233d10e9b147

Fix DebugInfoActivity - was crashing when fetching services
The issue was introduced in df94fde5ff45a1aae6ff0a3abc76241b546feefa
because of a bad refactor by android-studio.

app/src/main/java/com/etesync/syncadapter/model/ServiceDB.java
@@ -92,7 +92,7 @@ public class ServiceDB {
             db.beginTransactionNonExclusive();
 
             // iterate through all tables
-            Cursor cursorTables = db.query("sqlite_master", new String[]{"name"}, "enumType='table'", null, null, null, null);
+            Cursor cursorTables = db.query("sqlite_master", new String[]{"name"}, "type='table'", null, null, null, null);
             while (cursorTables.moveToNext()) {
                 String table = cursorTables.getString(0);
                 sb.append(table).append("\n");